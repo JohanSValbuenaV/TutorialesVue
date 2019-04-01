545fcd14d4f0663a628fe1853a9a9a40b68c5fef
Johan S Valbuena <u1201847@unimilitar.edu.co>
2019-04-01 14:08:56 -0500

    Acciones

--- PruebaVueAvanced/contenedor.js
+++ PruebaVueAvanced/contenedor.js
@@ -1,6 +1,7 @@
 const store = new Vuex.Store({
     state:{
-        numero:10
+        numero:10,
+        cursos:[]
     },
     mutations:{
         aumentar(state){
@@ -8,6 +9,16 @@ const store = new Vuex.Store({
         },
         disminuir(state,n){
             state.numero -= n
+        },
+        llenarCursos(state,cursonAccion){
+            state.cursos = cursonAccion
+        }
+    },
+    actions:{
+        obtenercursos: async function ({commit}){
+            const data = await fetch('cursos.json');
+            const cursos = await data.json();
+            commit('llenarCursos',cursos) 
         }
     }
     
